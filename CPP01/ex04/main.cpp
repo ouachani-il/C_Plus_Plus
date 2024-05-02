84641babdaa7667e50be0a6c6f87cff47b936f8b
Ilham Ouachani <lahoum_o@hotmail.com>
2024-05-02 07:52:24 +0200

    ex04 CPP01

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ilouacha <ilouacha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ilham_oua <ilham_oua@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 11:13:27 by ilouacha          #+#    #+#             */
-/*   Updated: 2024/05/01 11:26:16 by ilouacha         ###   ########.fr       */
+/*   Updated: 2024/05/01 16:54:20 by ilham_oua        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,18 @@
 
 int	main(){
 	std::string	filename;
-	std::cout << "Please Enter a file name \t:" << std::flush;
-	std::cin >> filename ;
+	std::string	s1;
+	std::string	s2;
 	
+	std::cout << "Please enter the filename \t:" << std::flush;
+	std::cin >> filename ;
+	std::cout << std::endl;
+	std::cout << "Please enter the string to search and replace\t:" << std::flush;
+	std::cin >> s1;
+	std::cout << std::endl;
+	std::cout << "Please enter the string to replace with \t:" << std::flush;
+	std::cin >> s1;
+	std::cout << std::endl;
+
+
 }
