060aa5564f42a3882947b5c58d5b87992406b77e
Ilham Ouachani <ilouacha@student.42.fr>
2024-05-07 17:25:21 +0200

    cpp02 begin

---
+++ CPP02/ex00/Fixed.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilouacha <ilouacha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/07 16:01:23 by ilouacha          #+#    #+#             */
+/*   Updated: 2024/05/07 16:04:43 by ilouacha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _FIXED_HPP_
+# define _FIXED_HPP_
+
+# include <iostream>
+# include <string>
+class Fixed{
+	public:
+	Fixed();
+	~Fixed();
+	private:
+	
+};
+#endif
