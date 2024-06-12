d5e571fb2020190755fbbcc9cbb828bd08316bf7
Ilham Ouachani <lahoum_o@hotmail.com>
2024-06-12 13:10:04 +0200

    maj ex01

--- CPP04/ex01/main.cpp
+++ CPP04/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ilouacha <ilouacha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ilham_oua <ilham_oua@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 10:33:35 by ilouacha          #+#    #+#             */
-/*   Updated: 2024/06/11 17:37:55 by ilouacha         ###   ########.fr       */
+/*   Updated: 2024/06/12 11:44:14 by ilham_oua        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,24 @@
 
 int main()
 {
-const Animal* j = new Dog();
-const Animal* i = new Cat();
-delete j;//should not create a leak
-delete i;
-Animal*	animals;
+const	Animal* j = new Dog();
+const	Animal* i = new Cat();
+delete	j;//should not create a leak
+delete	i;
+unsigned int	N = 10;
+Animal	*animals[N];
 
-for (unsigned int k; k < N / 2; k++){
-	
+for (unsigned int k = 0; k < N / 2; k++){
+	animals[k] = new Cat();
+	animals[N / 2 + k + 1] = new Dog();
 }
+
+// Delete the allocated animals,
+delete[] animals;
+/*for (unsigned int k; k < N / 2; k++){
+	animals;
+	animals[N / 2 + k] = new Dog();
+}*/
+
 return 0;
 }
