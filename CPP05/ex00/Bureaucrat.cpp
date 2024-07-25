2b558c5282734337b4d8d76027c5764d0b1dc5fa
Ilham Ouachani <ilouacha@student.42.fr>
2024-07-25 23:56:16 +0200

    ex00

--- CPP05/ex00/Bureaucrat.cpp
+++ CPP05/ex00/Bureaucrat.cpp
@@ -6,11 +6,33 @@
 /*   By: ilouacha <ilouacha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 18:45:26 by ilouacha          #+#    #+#             */
-/*   Updated: 2024/07/24 18:52:56 by ilouacha         ###   ########.fr       */
+/*   Updated: 2024/07/25 23:32:15 by ilouacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(){
+	this->grade = 150;
+	this->name = "default";
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const & src)
+{
+	*this = src;
+}
+
+Bureaucrat&	Bureaucrat::operator=(Bureaucrat const & rhs){
+	if (this != &rhs){
+		this->name = rhs.getName();
+		this->grade = rhs.getGrade();
+		
+	}
+	return *this;
+}
+Bureaucrat::~Bureaucrat(){
+	
+}
 
 std::string		Bureaucrat::getName(){
 	return (this->name);
@@ -21,8 +43,36 @@ int				Bureaucrat::getGrade(){
 }
 
 void	Bureaucrat::incGrade(){
-	if (grade == 1)
-	{
-		catch
+	try{
+		if (grade == 1)
+		{
+			throw "No increment is possible" ;
+		}
 	}
+	catch(const std::out_of_range& e) {
+		std::cout << "Out of range exception: " << e.what() << std::endl;
+	}
+		
+}
+void	Bureaucrat::decGrade(){
+	try{
+		if (grade == 150)
+		{
+			throw "No decrement is possible" ;
+		}
+	}
+	catch(const std::out_of_range& e) {
+		std::cout << "Out of range exception: " << e.what() << std::endl;
+	}
+		
+}
+void	Bureaucrat::GradeTooHighException(){
+	
+}
+
+void	Bureaucrat::GradeTooLowException(){
+	
+}
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat){
+	os << bureaucrat.getName() << 
 }
--- CPP05/ex00/Bureaucrat.hpp
+++ CPP05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: ilouacha <ilouacha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 18:30:49 by ilouacha          #+#    #+#             */
-/*   Updated: 2024/07/24 18:51:59 by ilouacha         ###   ########.fr       */
+/*   Updated: 2024/07/25 23:34:52 by ilouacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,27 @@
 
 # include <iostream>
 # include <string>
+#
 
 class Bureaucrat{
 private:
-	std::string const	name;
+	std::string			name;
 	int					grade;
 public:
+	Bureaucrat(void);
+	//Bureaucrat(std::string n, int g);
+	Bureaucrat(Bureaucrat const & src);
+	Bureaucrat&			operator=(Bureaucrat const & rhs);
+	virtual 			~Bureaucrat(void);
+	
 	std::string	getName();
 	int			getGrade();
 	void		incGrade();
 	void		decGrade();
+	void		GradeTooHighException();
+	void		GradeTooLowException();
 };
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat);
 
 #endif
