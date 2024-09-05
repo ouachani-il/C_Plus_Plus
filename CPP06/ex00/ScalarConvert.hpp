55e94d85434b817c3a44bf3fe6f2f32d9c63ee68
Ilham Ouachani <ilouacha@student.42.fr>
2024-09-05 12:52:18 +0200

    ex00 stardted

---
+++ CPP06/ex00/ScalarConvert.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConvert.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilouacha <ilouacha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/05 12:42:23 by ilouacha          #+#    #+#             */
+/*   Updated: 2024/09/05 12:48:22 by ilouacha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALERCONVERT_HPP
+# define SCALARCONVERT_HPP
+
+# include <iostream>
+# include <string>
+
+class ScalarConvert {
+	private:
+	
+	ScalarConvert();
+	ScalarConvert(ScalarConvert const &src);
+	~ScalarConvert();
+	ScalarConvert	&operator=(ScalarConvert const &rhs);
+	
+	public:
+
+	
+};
+
+#endif
