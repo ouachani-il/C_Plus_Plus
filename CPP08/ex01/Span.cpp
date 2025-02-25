9fcc9f243d45cded55a79262acd532eb7c00b00d
Ilham OUACHANI <ilouacha@paul-f4Ar7s1.clusters.42paris.fr>
2025-02-25 21:00:43 +0100

    ex01 of cpp08

--- CPP08/ex01/Span.cpp
+++ CPP08/ex01/Span.cpp
@@ -6,32 +6,75 @@
 /*   By: ilouacha <ilouacha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 15:51:31 by ilouacha          #+#    #+#             */
-/*   Updated: 2025/02/19 15:59:11 by ilouacha         ###   ########.fr       */
+/*   Updated: 2025/02/25 20:57:41 by ilouacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
+#include <algorithm>
+#include <limits>
+#include <vector>
+#include <iterator>
 
-Span::Span(unsigned int N){
+
+Span::Span(unsigned int N) : _N(N){
+    if (N != 0)
+        _tab.reserve(N);
+}   
+Span::Span(Span const & src){
+    *this = src;
     
 }
-Span(Span const & src){
-    
-}
-Span& operator=(Span const & rhs){
-    
+Span& Span::operator=(Span const & rhs){
+    _N = rhs._N;
+    _tab = rhs._tab;
+    return (*this);
 }
 Span::~Span(){
-    
 }
 
-void    Span::addNumber(){
+void    Span::addNumber(int x){
+    if (_tab.size() < _N){
+        _tab.push_back(x);
+    }
+    else {
+        throw over_size();
+    }
     
 } //shortest distance between the numbers stacked
 int     Span::shortestSpan(){
+    if (_tab.size() <= 1){
+        throw numbers_not_enough();
+    }
+    else {
+        std::sort(_tab.begin(), _tab.end());
+        int min_dist = std::numeric_limits<int>::max();
+        std::vector<int>::iterator it;
+        for (it = _tab.begin(); it < _tab.end(); ++it ){
+            min_dist = std::min(min_dist, *it - *(it-1));
+        }
+        return (min_dist);
+    }
     
 }
 // longest distance between the numbers stacked
 int     Span::longestSpan(){
-    
+    if (_tab.size() <= 1){
+        throw numbers_not_enough();
+    }
+    else {
+        std::vector<int>::iterator it_min = std::min_element(_tab.begin(), _tab.end());
+        std::vector<int>::iterator it_max = std::max_element(_tab.begin(), _tab.end());
+        return (*it_max - *it_min);
+    }
+   
 }
+
+void    Span::addNumbers(std::vector<int>::const_iterator begin, 
+    std::vector<int>::const_iterator end){
+        if (_tab.size() + std::distance(begin, end) > _N){
+            throw over_size();
+        }
+        _tab.insert(_tab.end(), begin, end);
+    }
+  
--- CPP08/ex01/Span.hpp
+++ CPP08/ex01/Span.hpp
@@ -6,15 +6,16 @@
 /*   By: ilouacha <ilouacha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 22:19:11 by ilouacha          #+#    #+#             */
-/*   Updated: 2025/02/19 15:54:54 by ilouacha         ###   ########.fr       */
+/*   Updated: 2025/02/25 20:58:52 by ilouacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SPAN_HPP
-#define SPAN_hpp
+#define SPAN_HPP
 #include <iostream>
 #include <vector>
 #include <iterator>
+
 class Span {
   public:
     Span(unsigned int N);
@@ -22,11 +23,27 @@ class Span {
     Span& operator=(Span const & rhs);
     ~Span();
     
-    void    addNumber();
+    void    addNumber(int x);
+    void    addNumbers(std::vector<int>::const_iterator begin, 
+      std::vector<int>::const_iterator end);
     int     shortestSpan(); //shortest distance between the numbers stacked
     int     longestSpan(); // longest distance between the numbers stacked
+
+    class over_size : public std::exception{
+      public:
+        virtual const char* what() const throw(){
+          return ("Max size reached ! Cannot add !");
+        }
+    };
+    class numbers_not_enough: public std::exception{
+      public:
+        virtual const char* what() const throw(){
+          return ("Numbers are not enough !");
+        }
+    };
   private:
-    unsigned int    _N;  
+    unsigned int    _N;
+    std::vector<int> _tab;  
 };
 
 #endif
