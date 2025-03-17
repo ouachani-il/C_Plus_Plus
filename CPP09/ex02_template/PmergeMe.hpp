d3b8c5413ca317a33650ee356fb5604e146f5b4d
Ilham OUACHANI <ilouacha@paul-f4Ar5s10.clusters.42paris.fr>
2025-03-17 16:35:05 +0100

    duplicating the pgm for the two containers

--- CPP09/ex02_template/PmergeMe.hpp
+++ CPP09/ex02_template/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: ilouacha <ilouacha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 15:08:39 by ilouacha          #+#    #+#             */
-/*   Updated: 2025/03/17 16:27:43 by ilouacha         ###   ########.fr       */
+/*   Updated: 2025/03/17 16:22:51 by ilouacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,25 +28,27 @@
 // a template for the container, to adapt the sorting algorithm for both of the containers chosen to do it.
 template <typename Container>
 class PmergeMe {
+    Container container;
     public:
-    
-    // construtor in canonical form
-    PmergeMe();
-    PmergeMe(int argc, char **argv);
-    PmergeMe(PmergeMe const & src);
-    PmergeMe&   operator=(PmergeMe const & rhs);
-    ~PmergeMe();
-    
-    
-    void    sorting();
-    void    sort_time();
-    
-    typename Container::iterator begin();
-    typename Container::iterator end();
-    
+        typedef typename Container::value_type ValueType;
+        typedef typename Container::iterator Iterator;
+        
+        
+        // construtor in canonical form
+        PmergeMe();
+        PmergeMe(int argc, char **argv);
+        PmergeMe(PmergeMe const & src);
+        PmergeMe&   operator=(PmergeMe const & rhs);
+        ~PmergeMe();
+        
+        
+        void    sorting();
+        void    sort_time();
+        
+        typename Container::iterator begin();
+        typename Container::iterator end();
+        
     private:
-        std::vector<int>    _Vector;
-        std::deque<int>     _Deque;
         time_t  _start;
         time_t  _end;
         
@@ -64,7 +66,5 @@ template <typename Container>
 std::ostream& operator<<(std::ostream &os, const PmergeMe<Container> &obj);
 
 #include "PmergeMe.tpp"
-
-//A la palce d'upper_bound, il faudrait utiliser binary search !
 
 #endif
