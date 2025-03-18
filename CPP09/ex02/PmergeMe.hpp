ae0f7bb1af4edc232b0db558bf57123b4e4dc172
Ilham OUACHANI <ilouacha@paul-f4Ar3s2.clusters.42paris.fr>
2025-03-18 15:14:46 +0100

    ex02 with binary search

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: ilouacha <ilouacha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 15:08:39 by ilouacha          #+#    #+#             */
-/*   Updated: 2025/03/17 16:27:43 by ilouacha         ###   ########.fr       */
+/*   Updated: 2025/03/18 15:12:54 by ilouacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,44 +26,53 @@
 #include <ctime>
 
 // a template for the container, to adapt the sorting algorithm for both of the containers chosen to do it.
-template <typename Container>
 class PmergeMe {
     public:
     
-    // construtor in canonical form
+    // ****************construtor in canonical form
     PmergeMe();
-    PmergeMe(int argc, char **argv);
+    //PmergeMe(std::vector<unsigned int> Vect);
     PmergeMe(PmergeMe const & src);
     PmergeMe&   operator=(PmergeMe const & rhs);
     ~PmergeMe();
     
-    
-    void    sorting();
-    void    sort_time();
-    
-    typename Container::iterator begin();
-    typename Container::iterator end();
-    
-    private:
-        std::vector<int>    _Vector;
-        std::deque<int>     _Deque;
-        time_t  _start;
-        time_t  _end;
+    std::vector<unsigned int>   getVector() const;
+    std::deque<unsigned int>    getDeque() const;
         
-        float   time();
+    void    parsing(int ac, char **av);
+    void    sorting();
+    void    sort_timeV();
+    void    sort_timeD();
+    void    print_results(int ac, char **av);
+    int     binary_search(std::vector<unsigned int > &vecteur, unsigned int b);
+        
+    private:
+        std::vector<unsigned int>    _Vector;
+        std::deque<unsigned int>     _Deque;
+        time_t  _startV;
+        time_t  _endV;
+        time_t  _startD;
+        time_t  _endD;
+        
+        float   timeV();
+        float   timeD();
         int     Jacobsthal(int i);
-        void    insert(Container &main, Container &pend, ValueType odd, Container &left, Container &vec, bool is_odd, int order);
-        void    sorting(Container & vec);
+        // vector sorting
+        void    insert(std::vector<unsigned int> &main, std::vector<unsigned int> &pend, unsigned int odd, std::vector<unsigned int> &left, 
+                    std::vector<unsigned int> &vec, bool is_odd, unsigned int order);
+        void    sorting(std::vector<unsigned int> & vec);
+        // Deque sorting
+        void    insert(std::deque<unsigned int> &main, std::deque<unsigned int> &pend, unsigned int odd, std::deque<unsigned int> &left, 
+                    std::deque<unsigned int> &vec, bool is_odd, unsigned int order);
+        void    sorting(std::deque<unsigned int> & vec);
         size_t  count_word_and_check(std::string &str) const;
-        std::string container_type();
     
 };
 
 // surcherge d'operateur << pour l'affichage du contenu du container
-template <typename Container>
-std::ostream& operator<<(std::ostream &os, const PmergeMe<Container> &obj);
+std::ostream& operator<<(std::ostream &os, std::vector<unsigned int> & obj);
+std::ostream& operator<<(std::ostream &os, std::deque<unsigned int> & obj);
 
-#include "PmergeMe.tpp"
 
 //A la palce d'upper_bound, il faudrait utiliser binary search !
 
--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ilouacha <ilouacha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 15:54:40 by ilouacha          #+#    #+#             */
-/*   Updated: 2025/03/17 12:24:26 by ilouacha         ###   ########.fr       */
+/*   Updated: 2025/03/18 13:43:43 by ilouacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,20 @@
 #include <cstdlib> 
 #include <ctime>
 
+
 int main(int argc, char **argv){
     try{
         if (argc > 2){
-            PmergeMe< std::vector<int> >   Vector(argc, argv);
-            PmergeMe< std::deque<int> >   Deque(argc, argv);
-            std::cout << "Before: " << Deque << std::endl;
-            Deque.sorting();
-            Vector.sorting();
-            std::cout << "After: " << Deque << std::endl;
-            
-            Deque.sort_time();
-            Vector.sort_time();
-        }
+            PmergeMe object;
+
+            object.print_results(argc , argv);
+                }
         else{
             throw "Error: bad inputs.";
         }
     }
     catch(...){
-        std::cerr << "Error: bad sorting " << std::endl;
+        std::cerr << "Error: bad trip " << std::endl;
     }
     return 0;
 }
